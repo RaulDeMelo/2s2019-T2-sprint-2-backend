88a18b5b29c694a368cea55ade00276cc6e9c656
RaulDeMelo <48664318+RaulDeMelo@users.noreply.github.com>
2019-09-16 13:13:41 -0300

    Update ICategoriaRepositorio.cs

--- ExOpflix/Senai.Opflix.WebApi/Senai.Opflix.WebApi/Interface/ICategoriaRepositorio.cs
+++ ExOpflix/Senai.Opflix.WebApi/Senai.Opflix.WebApi/Interface/ICategoriaRepositorio.cs
@@ -1,1 +1,12 @@
-a
+using Senai.Opflix.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Opflix.WebApi.Interface
+{
+    interface ILancamentoRepositorio
+    {
+      void Cadastrar();
+    }
